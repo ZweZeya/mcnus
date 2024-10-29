6f9a4c39893d86b9e5cdc12a0aebeaa9245b0748
zwe <zwezeya02@gmail.com>
2024-10-29 20:08:07 +0800

    add hero img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+
 export default function Home() {
     return (
-        <div>
-
+        <div className="flex justify-center my-4">
+            <div className="overflow-hidden border border-transparent rounded-[85px]">
+                <Image src="/hero.jpg" alt="hero" width="1350" height="1350" objectFit="cover" />
+            </div>
         </div>
     );
 }
