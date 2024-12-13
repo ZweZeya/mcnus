4abfd71348d01b5b68834a489fdfc589585df527
zwe <zwezeya02@gmail.com>
2024-12-14 00:24:11 +0800

    add navbar label indicator

--- src/app/components/navbar/NavItem.tsx
+++ src/app/components/navbar/NavItem.tsx
@@ -1,20 +1,29 @@
 "use client";
-import { useState } from "react"
-import { navy, white } from "../../utils/colors"
-import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react"
+import { frangipani, navy, white } from "../../utils/colors"
+import { useRouter, usePathname } from "next/navigation";
 
 const NavItem: React.FC<{name: string, to: string}> = ({name, to}) => {
     const [isHover, setHover] = useState<boolean>();
-    const router = useRouter()
+    const [isSelected, setSelected] = useState<boolean>(false);
+    const router = useRouter();
+    const pathname = usePathname();
+
+    useEffect(() => {
+        setSelected(pathname == to);
+
+    }, [pathname, to])
 
     const handleClick = () => {
-        router.push(to)
+        if (to != pathname) {
+            router.push(to);
+        }
     }
 
     return (
         <div 
-            className="cursor-pointer"
-            style={{color: isHover ? white : navy}} 
+            className="cursor-pointer h-full flex flex-col justify-center px-3"
+            style={{color: !isSelected && isHover ? white : navy, backgroundColor: isSelected ? white : frangipani}} 
             onMouseEnter={() => setHover(true)} 
             onMouseLeave={() => setHover(false)}
             onClick={handleClick}
--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -6,13 +6,13 @@ import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <div className="flex items-center py-4 px-6" style={{backgroundColor: frangipani, height: "7vh"}}>
+        <div className="flex items-center px-6" style={{backgroundColor: frangipani, height: "7vh"}}>
             <Link href="/">
                 <Logo size={40} />
             </Link>
-            <div className="flex gap-x-5 ml-auto">
+            <div className="flex gap-5 items-center ml-auto h-full">
                 <NavItem name={S.whoWeAre} to="/about" />
-                <NavItem name={S.events} to="/"/>
+                <NavItem name={S.events} to="/events"/>
                 <NavItem name={S.joinUs} to="/recruitment"/>
                 <NavItem name={S.contactUs}to="/contact" />
             </div>
