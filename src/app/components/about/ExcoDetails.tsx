09d56aaa4e2fa86466fb084257b43783c33dff9f
zwe <zwezeya02@gmail.com>
2024-12-13 21:29:02 +0800

    add about page

--- src/app/components/about/ExcoDetails.tsx
+++ src/app/components/about/ExcoDetails.tsx
@@ -1,20 +1,20 @@
 import { promises as fs } from 'fs';
 import ColouredContentBox from "../common/ColouredContentBox";
 import S from "@/app/utils/constantString";
-
-interface ExcoMember {
-    name: string;
-}
+import ExcoMemberCard, { ExcoMember } from './ExcoMemberCard';
 
 const ExcoDetails = async ()  => {
     const filePath = process.cwd() + '/src/data/exco.json';
     const file = await fs.readFile(filePath, 'utf8');
     const data: ExcoMember[] = JSON.parse(file);
-    console.log(data);
+    
+    const excoMembers = data.map((e, i) => <ExcoMemberCard key={i} member={e} />);
 
     return (
         <ColouredContentBox className="w-full text-center" title={S.executiveCommittee}>
-
+            <div className="w-full h-full flex flex-wrap items-center justify-center gap-10">
+                { excoMembers }
+            </div>
         </ColouredContentBox>
     )
 }
--- src/app/components/about/ExcoMemberCard.tsx
+++ src/app/components/about/ExcoMemberCard.tsx
@@ -1,6 +1,21 @@
-const ExcoMemberCard = () => {
+import { Text } from "../common/textComponents";
+import Image from "next/image";
+
+export interface ExcoMember {
+    name: string;
+    role: string;
+    image: string;
+}
+
+const ExcoMemberCard: React.FC<{member: ExcoMember}> = ({member}) => {
     return (
-        <div></div>
+        <div className="relative w-1/4 flex flex-col items-center gap-2">
+            <div className="overflow-hidden relative border border-transparent rounded-full w-36 h-36">
+                <Image src={member.image} alt={member.name} fill objectFit="cover" />
+            </div>
+            <Text>{member.name}</Text>
+            <Text>{member.role}</Text>
+        </div>
     )
 }
 
