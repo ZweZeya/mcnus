72d009d210ed4d817c1ca717fec7bc7d7560ee60
zwe <zwezeya02@gmail.com>
2024-12-14 09:55:32 +0800

    remove blank lines

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,8 +2,6 @@ import ContentBox from "../components/common/ContentBox";
 import PageLayout from "../components/layout/PageLayout"
 import S from "../utils/constantString";
 
-  
-
 const EventsPage = () => {
     return (
         <PageLayout>
