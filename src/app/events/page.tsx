256eea02f2b388b1b9c93706895d1bff124753a5
zwe <zwezeya02@gmail.com>
2024-12-14 09:55:15 +0800

    add events page boilerplate

---
+++ src/app/events/page.tsx
@@ -0,0 +1,15 @@
+import ContentBox from "../components/common/ContentBox";
+import PageLayout from "../components/layout/PageLayout"
+import S from "../utils/constantString";
+
+  
+
+const EventsPage = () => {
+    return (
+        <PageLayout>
+            <ContentBox title={S.events} content="" />
+        </PageLayout>
+    )
+}
+
+export default EventsPage;
