e811f6cae97fec789465be809ebfd1dbc75c0963
zwe <zwezeya02@gmail.com>
2024-12-16 16:37:28 +0800

    make text responsive

--- src/app/components/layout/PageLayout.tsx
+++ src/app/components/layout/PageLayout.tsx
@@ -1,6 +1,6 @@
 const PageLayout = (props: React.PropsWithChildren) => {
     return (
-        <div className="flex flex-col items-center py-8 px-32">
+        <div className="flex flex-col items-center py-8 px-3 md:px-20 xl:px-32">
             {props.children}
         </div>
     )
