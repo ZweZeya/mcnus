42259e61691e4b40ad3173df62b51b8c690ffab2
zwe <zwezeya02@gmail.com>
2024-12-17 15:34:58 +0800

    change hero image resizing

--- src/app/components/home/Hero.tsx
+++ src/app/components/home/Hero.tsx
@@ -6,12 +6,12 @@ import ContentBox from "../common/ContentBox";
 
 const Hero = () => {
     return (
-        <div style={{height: "86vh"}}>
+        <div>
             <div className="relative w-full h-3/5 flex flex-col items-center">
                 <div className="overflow-hidden border border-transparent rounded-[85px] relative w-full h-full">
-                    <Image src="/hero.jpg" alt="hero" fill objectFit="cover" />
+                    <Image src="/hero.jpg" alt="hero" width={5000} height={550} objectFit="cover" />
                 </div>
-                <div className="absolute top-1/2 w-5/6">
+                <div className="absolute top-1/3 lg:top-1/2 w-5/6">
                     <p className={`${dynalight.className} text-center sm:text-[28px] md:text-[70px] xl:text-[76px]`} style={{color: frangipani}}>
                         {S.motto}
                     </p>
