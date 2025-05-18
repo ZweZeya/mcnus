0a3721fa139cc5bddfd3a5ac3d8eba42491869e4
Lhanxi <118047094+Lhanxi@users.noreply.github.com>
2025-05-18 21:54:38 +0800

    Fixed footer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -3,19 +3,56 @@ import { FaInstagram, FaTelegram, FaLinkedin, FaFacebook, FaYoutube } from "reac
 import FooterItem from "./FooterItem";
 import Logo from "../Logo";
 import S from "@/app/utils/constantString";
+import { MenuItem } from "../navbar/Menu";
+import FooterLink from "./FooterLink";
 
 const Footer = () => {
+    const menuItems: MenuItem[] = [
+        { name: S.whoWeAre, to: "/about" },
+        { name: S.joinUs, to: "/recruitment" },
+    ];
     return (
-        <div className="flex items-center py-4 px-6 mt-auto justify-center" style={{backgroundColor: frangipani, height: "7vh"}}>
-            <div className="hidden md:inline">
-                <Logo size={40} />
+        <div className="flex items-center py-4 px-6 mt-auto" style={{ backgroundColor: frangipani }}>
+            {/* Left Portion */}
+            <div className="flex-1 flex justify-center">
+                <div className="flex flex-col text-left">
+                    <div className="flex items-center mb-1">
+                        <Logo size={40} />
+                        <strong className="ml-2">MC@NUS</strong>
+                    </div>
+                    <p className="text-sm">
+                        Myanmar Community @ National University of Singapore<br />
+                        A community established by Myanmar Students in NUS for NUS networks.
+                    </p>
+                </div>
             </div>
-            <div className="flex gap-x-5 md:ml-auto">
-                <FooterItem icon={<FaInstagram size={30} />} to={S.instagramLink}/>
-                <FooterItem icon={<FaTelegram size={30} />} to={S.telegramLink} />
-                <FooterItem icon={<FaLinkedin size={30} />} to={S.linkedinLink} />
-                <FooterItem icon={<FaFacebook size={30} />} to={S.facebookLink} />
-                <FooterItem icon={<FaYoutube size={30} />} to={S.youtubeLink} />
+
+            {/* Middle Portion */}
+            <div className="flex-1 flex justify-center">
+                <div className="flex flex-col text-center">
+                    <h3 className="font-semibold">Quick Links</h3>
+                    <ul className="text-sm space-y-1 mt-1">
+                        {menuItems.map((e, i) => (
+                            <li key={i}>
+                                <FooterLink name={e.name} to={e.to} />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+
+            {/* Right Portion */}
+            <div className="flex-1 flex justify-center">
+                <div className="flex flex-col text-left">
+                    <h3 className="font-semibold mb-2">Follow us</h3>
+                    <div className="flex gap-x-5">
+                        <FooterItem icon={<FaInstagram size={30} />} to={S.instagramLink} />
+                        <FooterItem icon={<FaTelegram size={30} />} to={S.telegramLink} />
+                        <FooterItem icon={<FaLinkedin size={30} />} to={S.linkedinLink} />
+                        <FooterItem icon={<FaFacebook size={30} />} to={S.facebookLink} />
+                        <FooterItem icon={<FaYoutube size={30} />} to={S.youtubeLink} />
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,26 +1,26 @@
 import { frangipani } from "@/app/utils/colors";
-import NavItem from "./NavItem"
-import Logo from "../Logo"
+import NavItem from "./NavItem";
+import Logo from "../Logo";
 import S from "@/app/utils/constantString";
 import Link from "next/link";
 import Menu, { MenuItem } from "./Menu";
 
 const Navbar = () => {
     const menuItems: MenuItem[] = [
-        {name: S.whoWeAre, to: "/about"},
-        {name: S.events, to: "/events"},
-        {name: S.joinUs, to: "/recruitment"},
-        {name: S.contactUs, to: "/contact"}
+        { name: S.whoWeAre, to: "/about" },
+        { name: S.events, to: "/events" },
+        { name: S.joinUs, to: "/recruitment" },
+        { name: S.contactUs, to: "/contact" }
     ];
 
     return (
-        <div className="flex items-center px-6" style={{backgroundColor: frangipani, height: "7vh"}}>
+        <div className="flex items-center px-6" style={{ backgroundColor: frangipani, height: "7vh" }}>
             <Link href="/">
                 <Logo size={40} />
             </Link>
             <div className="ml-auto h-full flex items-center">
                 <div className="gap-5 items-center h-full hidden md:flex">
-                    { menuItems.map((e, i) => <NavItem key={i} name={e.name} to={e.to} />) }
+                    {menuItems.map((e, i) => <NavItem key={i} name={e.name} to={e.to} />)}
                 </div>
                 <div className="md:hidden">
                     <Menu items={menuItems} />
