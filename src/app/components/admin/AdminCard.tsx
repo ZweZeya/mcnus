34b5dc00fa5c936db9602eae7596fe36fe60e501
Win Paing Oo <winpaing000@gmail.com>
2026-05-13 14:40:38 +0800

    Remove hover from admin card

--- src/app/components/admin/AdminCard.tsx
+++ src/app/components/admin/AdminCard.tsx
@@ -5,8 +5,6 @@ import { useRouter, usePathname } from "next/navigation"
 import { frangipani } from "@/app/resources/colors"
 
 const AdminCard: React.FC<{ name: string, to: string }> = ({ name, to }) => {
-    const [isHover, setHover] = useState<boolean>(false)
-    
     const router = useRouter()
     const pathname = usePathname()
 
@@ -19,9 +17,7 @@ const AdminCard: React.FC<{ name: string, to: string }> = ({ name, to }) => {
     return (
         <div
         className="cursor-pointer w-[340px] h-40 px-4 py-3 rounded-xl shadow-md flex items-center gap-4 justify-center hover:shadow-lg hover:scale-105"
-        style={{ backgroundColor: frangipani}}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
+        style={{ backgroundColor: frangipani }}
         onClick={handleCLick}>
             {name}
         </div>
