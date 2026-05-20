ac38cc2c883f9fa5afd9ee6236e7bac2be58cae5
Win Paing Oo <winpaing000@gmail.com>
2026-05-20 23:02:17 +0800

    Update modal close button

--- src/app/components/events/SingleEventModal.tsx
+++ src/app/components/events/SingleEventModal.tsx
@@ -44,7 +44,7 @@ const SingleEventModal: React.FC<{ eventId: number, onClose: () => void}> = ({ e
         <div className="fixed inset-0 z-50 flex items-center justify-center">
             <div className="absolute inset-0 bg-black/65 flex items-center justify-center">
                 <div className="relative w-full max-w-lg rounded-2xl shadow-2xl z-10  h-auto overflow-hidden" style={{ backgroundColor: grey}}>
-                    <div className="absolute top-0 right-0 p-3 z-50">
+                    <div className="absolute top-0 right-0 z-50">
                         <IoCloseCircle size={45} onClick={onClose} className="cursor-pointer hover:opacity-100 opacity-50"/>
                     </div>
                     <div className="overflow-y-auto max-h-[90vh] w-full p-4 ">
