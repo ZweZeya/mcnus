7eeb1453b633db8a4dcdd5436af37ce2ef2875af
Win Paing Oo <winpaing000@gmail.com>
2026-05-20 14:54:40 +0800

    Remove dummy description

--- src/app/components/events/SingleEventModal.tsx
+++ src/app/components/events/SingleEventModal.tsx
@@ -45,7 +45,7 @@ const SingleEventModal: React.FC<{ eventId: number, onClose: () => void}> = ({ e
             <div className="absolute inset-0 bg-black/65 flex items-center justify-center">
                 <div className="relative w-full max-w-lg rounded-2xl shadow-2xl z-10  h-auto overflow-hidden" style={{ backgroundColor: grey}}>
                     <div className="absolute top-0 right-0 p-3 z-50">
-                        <IoCloseCircle size={40} onClick={onClose} className="cursor-pointer hover:opacity-100 opacity-30"/>
+                        <IoCloseCircle size={45} onClick={onClose} className="cursor-pointer hover:opacity-100 opacity-50"/>
                     </div>
                     <div className="overflow-y-auto max-h-[90vh] w-full p-4 ">
                         <div className="relative w-full pb-[100%]">
@@ -67,7 +67,6 @@ const SingleEventModal: React.FC<{ eventId: number, onClose: () => void}> = ({ e
                             </div>
                             <div className="text-left text-lg pb-4">
                                 {event.description}
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
                             </div>
                             <div className="flex flex-col gap-3">
                                 <Button
