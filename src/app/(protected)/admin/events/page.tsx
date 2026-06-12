e4bbdffaba5b9e114f3cebb15f6a6e8dcb99f2b1
Win Paing Oo <winpaing000@gmail.com>
2026-06-12 09:31:30 +0800

    Add server side fetching for admin events

--- src/app/(protected)/admin/events/page.tsx
+++ src/app/(protected)/admin/events/page.tsx
@@ -3,7 +3,6 @@ import AdminEvents from "@/app/components/admin/AdminEvents";
 export default function AdminEventsPage() {
   return (
     <main>
-      {/* We simply import and render the component here */}
       <AdminEvents /> 
     </main>
   );
